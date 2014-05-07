4fbbe0120bbdd10f094654b4067e5c27fe699e79
Jason Kim <active_red@hotmail.com>
2014-05-06 17:43:17 -0700

    close #5: Sticky footer with links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def ApplicationHelper.app_name
     'WellRead'
   end
+
+  def app_name
+    'WellRead'
+  end
 end
--- spec/views/pages/home_spec.rb
+++ spec/views/pages/home_spec.rb
@@ -4,15 +4,28 @@ describe '#home' do
   before { visit root_path }
 
   it 'has correct links in navigation bar' do
-    expect(page).to have_link(ApplicationHelper.app_name)
-    expect(page).to have_link('Categories')
-    expect(page).to have_link('Log In')
-    expect(page).to have_link('Sign Up')
+    within('.navbar') do
+      expect(page).to have_link(ApplicationHelper.app_name)
+      expect(page).to have_link('Categories')
+      expect(page).to have_link('Log In')
+      expect(page).to have_link('Sign Up')
 
-    expect(page).to have_link('Technology')
-    expect(page).to have_link('Politics')
-    expect(page).to have_link('Culture')
-    expect(page).to have_link('Business')
-    expect(page).to have_link('Meta')
+      expect(page).to have_link('Technology')
+      expect(page).to have_link('Politics')
+      expect(page).to have_link('Culture')
+      expect(page).to have_link('Business')
+      expect(page).to have_link('Meta')
+    end
+  end
+
+  it 'has correct links in the footer' do
+    within('#footer') do
+      expect(page).to have_link(ApplicationHelper.app_name)
+      expect(page).to have_link('About')
+      expect(page).to have_link('Help')
+      expect(page).to have_link('Privacy Policy')
+      expect(page).to have_link('User Agreement')
+      expect(page).to have_link('Contact')
+    end
   end
 end
