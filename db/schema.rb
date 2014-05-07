673b9d220c94d64dd5eb0464f07e60f03c3730e8
Jason Kim <active_red@hotmail.com>
2014-05-06 22:30:24 -0700

    close #6 url to link

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140506231924) do
+ActiveRecord::Schema.define(version: 20140507052831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20140506231924) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "url"
   end
 
   create_table "userlinkables", force: true do |t|
