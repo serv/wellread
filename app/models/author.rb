c55482df05728463b8767a76b7738f0f8f7b8715
Jason Kim <active_red@hotmail.com>
2014-05-09 16:53:10 -0700

    close #22: author validation

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,6 +2,13 @@ class Author < ActiveRecord::Base
   has_many :authorables
   has_many :links, through: :authorables
 
+  validates :name, presence: true
+  validates :name, length: { maximum: 140 }
+  validates :name, uniqueness: true
+  validates :name, format: {
+    with: /\A[a-zA-Z\s]+\z/,
+    message: "only allows letters" }
+
   def self.search_by_name(name)
     Author.where('name ~* ?', name)
   end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -3,4 +3,10 @@ require 'spec_helper'
 describe Author do
   it { should have_many(:authorables) }
   it { should have_many(:links).through(:authorables) }
+
+  it { should validate_presence_of(:name) }
+  it { should ensure_length_of(:name).is_at_most(140) }
+  it { should validate_uniqueness_of(:name) }
+  it { should allow_value('Jason Kim', 'Mark Zuck').for(:name) }
+  it { should_not allow_value('J3s0n K1m').for(:name) }
 end
