5da21f9ceccdbf7cd1182cf6fcf46b66239eca69
Jason Kim <active_red@hotmail.com>
2014-05-11 00:56:15 -0700

    updated gemfile for cap

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+role :app, %w{jkim@107.170.199.162}
+role :web, %w{jkim@107.170.199.162}
+role :db,  %w{jkim@107.170.199.162}
 
 
 # Extended Server Syntax
@@ -15,7 +15,7 @@ role :db,  %w{deploy@example.com}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server '107.170.199.162', user: 'jkim', roles: %w{web app}, my_property: :my_value
 
 
 # Custom SSH Options
